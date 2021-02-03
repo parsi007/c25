597f8ed634e345cfc3bef5ebc361047e52dc563c
parsi007 <76106066+parsi007@users.noreply.github.com>
2021-02-03 18:04:59 +0530

    Add files via upload

---
+++ Box.js
@@ -0,0 +1,8 @@
+class Box extends Baseclass {
+  constructor(x, y, width, height) {
+    super(x,y,width,height);
+    this.image=loadImage("sprites/wood1.png");
+    
+  }
+  
+};
---
+++ baseclass.js
@@ -0,0 +1,31 @@
+class Baseclass {
+    constructor(x, y, width, height) {
+      var options = {
+          'restitution':0.8,
+          'friction':1.0,
+          'density':1.0
+      }
+      this.body = Bodies.rectangle(x, y, width, height, options);
+      this.width = width;
+      this.height = height;
+      this.image=loadImage("sprites/base.png");
+      
+      World.add(world, this.body);
+    }
+    display(){
+      var pos =this.body.position;
+      var angle = this.body.angle;
+      push();
+      translate(pos.x, pos.y);
+      rotate(angle);
+     // rectMode(CENTER);
+     // strokeWeight(4);
+      //stroke("green");
+      //fill(255);
+     // rect(0, 0, this.width, this.height);
+     imageMode(CENTER);
+     image(this.image,0,0,this.width,this.height);
+      pop();
+    }
+  };
+  
